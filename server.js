3f748b4455a59a60276b9552cc0bcdd1dc912400
benjipott <pott.benjamin@gmail.com>
2015-07-22 19:41:23 +0200

    make compatibility for heroku server

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  path = require('path'),
+  errorHandler = require('errorhandler'),
+  methodOverride = require('method-override'),
+  hostname = process.env.HOSTNAME || 'localhost',
+  port = process.env.PORT || 3000,
+  publicDir = path.join(__dirname, '/public');
+
+app.get('/', function (req, res) {
+  res.redirect('/index.html');
+});
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(publicDir));
+
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log('Simple static server showing %s listening at http://%s:%s', publicDir, hostname, port);
+app.listen(port, hostname);
